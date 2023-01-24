a555d0108dcc0815d24f3650ceb59c008196978f
tmolik <molik94@gmail.com>
2023-01-24 20:42:31 +0100

    FPSCameraController and PlayerMovementController, for starters

---
+++ Walking simulator/Assets/Scripts/Player/FPSCameraController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FPSCameraController : MonoBehaviour
+{
+
+    [SerializeField] private Transform _playerTransform = null;
+    [SerializeField] private Transform _cameraPositionTransform = null;
+
+    [SerializeField] private float _cameraSensitivity = 3;
+    [SerializeField] private float _maxUpAngle = 80;
+    [SerializeField] private float _maxDownAngle = -80;
+
+    private float _mouseX;
+    private float _mouseY;
+
+    private float _rotX = 0.0f;
+    private float _rotY = 0.0f;
+    private float _rotZ = 0.0f;
+
+
+    private Camera _cameraComponent;
+
+    private void Awake()
+    {
+        InitializeComponents();
+    }
+
+    private void InitializeComponents()
+    {
+        _cameraComponent = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // Mouse input
+        _mouseX = Input.GetAxis("Mouse X") * _cameraSensitivity;
+        _mouseY = Input.GetAxis("Mouse Y") * _cameraSensitivity;
+
+        // Calculations
+        _rotX -= _mouseY;
+        _rotX = Mathf.Clamp(_rotX, _maxDownAngle, _maxUpAngle);
+        _rotY += _mouseX;
+
+        // Placing values
+        transform.localRotation = Quaternion.Euler(_rotX, _rotY, _rotZ);
+        _playerTransform.Rotate(Vector3.up * _mouseX);
+        transform.position = _cameraPositionTransform.position;
+    }
+
+    public void Shake(float magnitude, float duration)
+    {
+        StartCoroutine(IShake(magnitude, duration));
+    }
+
+    private IEnumerator IShake(float mag, float dur)
+    {
+        WaitForEndOfFrame wfeof = new WaitForEndOfFrame();
+        for (float t = 0.0f; t <= dur; t += Time.deltaTime)
+        {
+            _rotZ = Random.Range(-mag, mag) * (t / dur - 1.0f);
+            yield return wfeof;
+        }
+        _rotZ = 0.0f;
+    }
+}
---
+++ Walking simulator/Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+
+    [Header("Movement properties")]
+
+    [SerializeField] private float _walkSpeed = 8f;
+    [SerializeField] private float _runSpeed = 12f;
+    [SerializeField] private float _maximumPlayerSpeed = 150.0f;
+
+    [Header("Jump")]
+    [SerializeField] private float _jumpForce = 500.0f;
+    [SerializeField] private float _jumpCooldown = 1.0f;
+    private bool _jumpBlocked = false;
+
+    [Header("GroundCheck")]
+    [SerializeField] private Transform _groundChecker;
+    [SerializeField] private float _groundCheckerDist = 0.2f;
+
+    private Rigidbody _playerRigidbody;
+
+    private float _verticalInput;
+    private float _horizontalInput;
+    private float _previousYVelocity;
+
+    private Vector3 _inputForce;
+    private bool _isGrounded = true;
+
+    private void Awake()
+    {
+        InitializeComponents();
+    }
+
+    private void FixedUpdate()
+    {
+        SetIsGrounded();
+        SetInputValues();
+        HandleMovement();
+    }
+
+    private void InitializeComponents()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void SetIsGrounded()
+    {
+        _isGrounded = (Mathf.Abs(_playerRigidbody.velocity.y - _previousYVelocity) < .1f) && (Physics.OverlapSphere(_groundChecker.position, _groundCheckerDist).Length > 1); // > 1 because it also counts the player
+        _previousYVelocity = _playerRigidbody.velocity.y;
+    }
+
+    private void SetInputValues()
+    {
+        _verticalInput = Input.GetAxisRaw("Vertical");
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void HandleMovement()
+    {
+        _playerRigidbody.velocity = ClampMag(_playerRigidbody.velocity, _maximumPlayerSpeed);
+
+        _inputForce = (transform.forward * _verticalInput + transform.right * _horizontalInput).normalized * (Input.GetKey(KeyCode.LeftShift) ? _runSpeed : _walkSpeed);
+
+        if (_isGrounded)
+        {
+            // Jump
+            if (Input.GetButton("Jump") && !_jumpBlocked)
+            {
+                _playerRigidbody.AddForce(-_jumpForce * _playerRigidbody.mass * Vector3.down);
+                _jumpBlocked = true;
+                Invoke(nameof(UnblockJump), _jumpCooldown);
+            }
+            // Ground controller
+            _playerRigidbody.velocity = Vector3.Lerp(_playerRigidbody.velocity, _inputForce, 10 * Time.fixedDeltaTime);
+        }
+        else
+        {
+            _playerRigidbody.velocity = ClampSqrMag(_playerRigidbody.velocity + _inputForce * Time.fixedDeltaTime, _playerRigidbody.velocity.sqrMagnitude);
+        }
+    }
+
+    private void UnblockJump()
+    {
+        _jumpBlocked = false;
+    }
+
+    private static Vector3 ClampSqrMag(Vector3 vec, float sqrMag)
+    {
+        if (vec.sqrMagnitude > sqrMag)
+            vec = vec.normalized * Mathf.Sqrt(sqrMag);
+        return vec;
+    }
+
+    private static Vector3 ClampMag(Vector3 vector, float maxMag)
+    {
+        if (vector.sqrMagnitude > maxMag * maxMag)
+            vector = vector.normalized * maxMag;
+        return vector;
+    }
+
+
+}
