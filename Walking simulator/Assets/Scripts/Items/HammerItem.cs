cef9f336d38bd528405c52a5b817736fd91cf61e
tmolik <molik94@gmail.com>
2023-02-25 23:04:06 +0100

    Added placeholder hammer animation

--- Walking simulator/Assets/Scripts/Items/HammerItem.cs
+++ Walking simulator/Assets/Scripts/Items/HammerItem.cs
@@ -1,6 +1,41 @@
 
+using System.Collections;
+using UnityEngine;
+
 public class HammerItem : Item
 {
+    private bool _isUsingHammer = false;
+
+    private string _animationName = "HammerUse";
+
+    public override void UpdateItemBehaviour()
+    {
+        base.UpdateItemBehaviour();
+        CheckForUseHammerInput();
+    }
+
+    private void CheckForUseHammerInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !_isUsingHammer)
+        {
+            UseHammer();
+        }
+    }
+
+    private void UseHammer()
+    {
+        _isUsingHammer = true;
+        StartCoroutine(UseHammerRoutine());
+    }
+
+    private IEnumerator UseHammerRoutine()
+    {
+        PlayerSystemsManager.Instance.PlayerAnimator.CrossFade(_animationName, 0.1f);
+        yield return new WaitForSeconds(0.1f);
+        //
+        _isUsingHammer = false;
+    }
+
     public override void OnEquipItem()
     {
         base.OnEquipItem();
